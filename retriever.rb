57ffc574e0b7dc2779878e5696a0f18c83dd8ffc
Toshiaki Asai <toshi.alternative@gmail.com>
2016-08-27 19:02:38 +0900

    use MessageMixin

--- retriever.rb
+++ retriever.rb
@@ -40,7 +40,10 @@ module Plugin::RSS
   end
 
   class Item < Retriever::Model
+    include Retriever::Model::MessageMixin
+
     register :rss, name: "RSS Topic"
+
     self.keys = [[:guid, :string],
                  [:link, :string],
                  [:title, :string, true],
@@ -48,106 +51,18 @@ module Plugin::RSS
                  [:created, :time],
                  [:site, Plugin::RSS::Site, true],
                 ]
-    def links
-      []
-    end
-    alias :entity :links
-
-    def mentioned_by_me?
-      false
-    end
-
-    def favorite(_)
-      # Intentionally blank
-    end
-
-    def favorite?
-      false
-    end
-
-    def favorited_by
-      []
-    end
-
-    def favoritable?
-      false
-    end
-
-    def retweet
-      # Intentionally blank
-    end
-
-    def retweet?
-      nil
-    end
-
-    def retweeted?
-      false
-    end
-
-    def retweeted_by
-      []
-    end
-
-    def retweetable?
-      false
-    end
-
-    def retweet_source(_=nil)
-      nil
-    end
-
-    def quoting?
-      false
-    end
-
-    def has_receive_message?
-      false
-    end
 
     def to_show
       @to_show ||= self[:title].gsub(/&(gt|lt|quot|amp);/){|m| {'gt' => '>', 'lt' => '<', 'quot' => '"', 'amp' => '&'}[$1] }.freeze
     end
 
-    def to_message
-      self
-    end
-    alias :message :to_message
-
-    def system?
-      false
-    end
-
-    def modified
-      created
-    end
-
-    def from_me?
-      false
-    end
-
-    def to_me?
-      true
-    end
-
     def user
-      site!
-    end
-
-    def idname
-      site!.idname
-    end
-
-    def repliable?
-      false
+      site
     end
 
     def perma_link
       link
     end
 
-    def receive_user_screen_names
-      []
-    end
   end
 end
